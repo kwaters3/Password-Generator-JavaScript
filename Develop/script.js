a641777192d7071084dba04e621f301522cefc7f
kwaters3 <knickler3@gmail.com>
2023-07-18 17:49:59 -0400

    added isNAN (not a number) in the function when selecting how many characters, these enforces the user to select an integar/number

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Alert to press the button to begin
-(alert ("To BEGIN - PRESS 'Generate Password' 😄                                            ***Press 🆗 to close this alert***"));
+(alert ("To BEGIN, PRESS 'Generate Password' 😄                                            ***Press 🆗 to close this alert***"));
 
 
 // Function to click the button to begin to process
@@ -10,12 +14,14 @@ function generatePassword () {
   // Open Console to see:
   console.log ("You Touched the Button!");
 
+
   // 1. Password Length
-  var passwordLength = prompt("How many characters would you like your password to have?");
-    if (passwordLength < 8 || passwordLength > 128) {
-        prompt("Uh, Oh! Your password must be between 8 and 128 characters.       Sorry, please try again. 😖");
-        return;
-        
+  var passwordLength = prompt("How many characters do you want in your password?");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert ("Uh, Oh! You must choose an integer/number between 8 and 128.       Sorry, please try again. 😖");
+      var passwordLength = prompt ("Please choose a number between 8 and 128.😊");
+      return;
+      
 }
 
 // 2. Variables for upperCase, lowerCase, Numbers, and Special Characters
@@ -23,13 +29,15 @@ var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 var numbers = ["0123456789"];
 var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\\]_`{|}~"];
+// used to concatonate when selecting these other variables
+var choiceCharacters = [];
 		
 
 
 //   
 //  
-// 2 - validate the input
-// 3 generate password based on criteria
+// 3 - validate the input
+// 4 generate password based on criteria
 
 
 
@@ -41,14 +49,15 @@ var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\\]_`{|}~"];
   return "final password";
 }
 
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
