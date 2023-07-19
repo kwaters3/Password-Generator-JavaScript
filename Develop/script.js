9325467285e93cf75fc6ab336ef9ba3a10d1566e
kwaters3 <knickler3@gmail.com>
2023-07-18 22:04:04 -0400

    Removed If statements due to error in the system not returning or producing password, also made each variable into it's own string

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,71 @@
+// Variables for lowerCase, upperCase, Numbers, and Special Characters
+var choiceCharacters = [];
+
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!","#","$","%","&","(",")", "*","+","-",".","/", ":",";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  }
+
+
+
 // Alert to press the button to begin
-(alert ("To BEGIN, PRESS 'Generate Password' 😄 \n***Press 🆗 to close this alert***"));
+(alert ("To BEGIN, 👉'Generate Password'👈 \n***Press 🆗 to close this alert*** 😄"));
+
 
 
 // Function to click the button to begin to process
 function generatePassword () {
-  // Open Console to see:
+
+
+// Open Console to see:
   console.log ("You Touched the Button!");
 
+// This is used to validate the input for the final password
+  var password = "";
 
-  // 1. Password Length
-  var passwordLength = prompt("How many characters do you want in your password?");
-    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-      alert ("Uh, Oh! You must choose an integer/number between 8 and 128. \nSorry, please try again. 😖");
-      var passwordLength = prompt ("Please choose a number between 8 and 128.😊");
 
+
+// 1. Password Length
+  var passwordLength = prompt("To begin the process, please specify the amount of characters you want in your password?");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert ("Uh, Oh! \nYou must choose an integer/number value between 8 and 128. \nSorry, please try again. 😖");
+    var passwordLength = prompt ("Please choose a number between 8 and 128.😊");
+   return "";
 }
 
-// 2. Variables for lowerCase, upperCase, Numbers, and Special Characters
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numbers = ["0123456789"];
-var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\\]_`{|}~"];
-// used to concatonate when selecting these other variables
-var choiceCharacters = [];
-var password = "";
+// 2. Lowercase, Uppercase, Numbers, Special Characters
+  var lower = confirm ("Do you want to include lower case 🔡 letters in your password? \nselect 'OK' for YES or 'CANCEL' for NO");
+  var upper = confirm ("Do you want to include UPPER case 🔠 letters in your password? \nselect 'OK' for YES or 'CANCEL' for NO");
+  var num = confirm ("Do you want to include Numbers 🔢 in your password? \nselect 'OK' for YES or 'CANCEL' for NO");
+  var special = confirm ("Do you want to include Special characters 🔣 in your password? \nselect 'OK' for YES or 'CANCEL' for NO");
+
+
 		
-
-// Selecting the variables- confirming or denying the input using Boolean
-if (confirm ("Do you want to include lower case 🔡 letters in your password? \nselect '🆗' for YES or 'CANCEL' for NO")) {
-  choiceCharacters = choiceCharacters.concat(lowerCase);
-}
-if (confirm ("Do you want to include UPPER case 🔠 letters in your password? \nselect '🆗' for YES or 'CANCEL' for NO")) {
-  choiceCharacters = choiceCharacters.concat(upperCase);
-}
-if (confirm ("Do you want to include Numbers 🔢 in your password? \nselect '🆗' for YES or 'CANCEL' for NO")) {
-  choiceCharacters = choiceCharacters.concat(numbers);
-}
-if (confirm ("Do you want to include Special characters 🔣 in your password? \nselect '🆗' for YES or 'CANCEL' for NO")) {
-  choiceCharacters = choiceCharacters.concat(specialCharacters);
-}
-
 
 // 3 - validate the input
 
-
-
-
-// 4 generate password based on criteria
+for (let i = 0; i < passwordLength; i++) {
+  var randomPassword = Math.floor(Math.random() * choiceCharacters.length);
+  password = password + choiceCharacters[random]
+  
+}
 
 
 
@@ -65,15 +77,4 @@ if (confirm ("Do you want to include Special characters 🔣 in your password? \
   return "final password";
 }
 
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-}
 
